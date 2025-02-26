4d98af58635685131f1ba4deeb6fa9c1ce4658d4
Jay9874 <jayprakashsharma225@gmail.com>
2025-02-26 18:55:54 +0530

    update

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,3 @@
-export default function handler(request) {
-  return new Response(JSON.stringify({ message: "Hello from Vercel, no framework!" }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+export function GET(request) {
+  return new Response('Hello from Vercel!');
 }
